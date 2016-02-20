749c008d3663f5983aa0c14c514ee40c1f0c5ba5
Eric Fung <eskfung@gmail.com>
2016-02-19 17:09:49 -0800

    Try adding libraries

---
+++ public/javascripts/require_config.js
@@ -0,0 +1,11 @@
+requirejs.config({
+  baseUrl: 'javascripts',
+  paths: {
+    jquery: [
+      '//code.jquery.com/jquery-2.2.0.min',
+      'vendor/jquery-2.2.0.min'
+    ],
+    classify: 'vendor/classify.min',
+    underscore: 'vendor/underscore-min'
+  }
+});
