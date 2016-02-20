3a58188636db00e4256c4ff47da4859a430e14d8
Eric Fung <eskfung@gmail.com>
2016-02-19 18:40:28 -0800

    Set up React and ReactDOM as dependencies

---
+++ assets/javascripts/mastermind/mastermind.jsx
@@ -0,0 +1,9 @@
+require([
+  'react',
+  'react-dom'
+], function(React, ReactDOM) {
+  ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('canvas')
+  );
+});
---
+++ public/javascripts/mastermind/mastermind.js
@@ -0,0 +1,7 @@
+require(['react', 'react-dom'], function (React, ReactDOM) {
+  ReactDOM.render(React.createElement(
+    'h1',
+    null,
+    'Hello, world!'
+  ), document.getElementById('canvas'));
+});
