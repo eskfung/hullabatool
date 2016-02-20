dbc30abd2cef8e942ab4f4e5a663764f94b8e348
Eric Fung <eskfung@gmail.com>
2016-02-19 17:58:21 -0800

    Begin a mastermind page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/mastermind', function(req, res, next) {
+  res.render('mastermind/index', { title: 'Mastermind' });
+});
+
 module.exports = router;
