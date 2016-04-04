f2994fc87984b84a21a0c13d1b79684b1677219f
Eric Fung <eskfung@gmail.com>
2016-04-04 16:30:14 -0700

    Oh, hello enzyme

--- test/peg_test.js
+++ test/peg_test.js
@@ -1,32 +1,27 @@
-var expect = require('chai').expect;
-var React = require('react');
-var TestUtils = require('react-addons-test-utils');
-var shallowRenderer = TestUtils.createRenderer();
-var Peg = require(__dirname + '/../assets/javascripts/mastermind/peg');
+import { expect } from 'chai';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Peg from '../assets/javascripts/mastermind/peg';
 
 describe('Peg', function() {
-  beforeEach(function() {
-    shallowRenderer.render(<Peg />);
-  });
-
   it('is a div', function(done) {
-    var pegComponent = shallowRenderer.getRenderOutput();
-    expect(pegComponent.type).to.equal('div');
+    var pegWrapper = shallow(<Peg />);
+    expect(pegWrapper.type()).to.equal('div');
 
     done();
   });
 
   it('has a peg classname', function(done) {
-    var pegComponent = shallowRenderer.getRenderOutput();
-    expect(pegComponent.props.className.split(' ').indexOf('peg')).to.not.equal(-1);
+    var pegWrapper = shallow(<Peg />);
+    expect(pegWrapper.props().className.split(' ').indexOf('peg')).to.not.equal(-1);
 
     done();
   });
 
   context('without a color', function() {
     it('has a peg-blank classname', function(done) {
-      var pegComponent = shallowRenderer.getRenderOutput();
-      expect(pegComponent.props.className.split(' ').indexOf('peg-blank')).to.not.equal(-1);
+      var pegWrapper = shallow(<Peg />);
+      expect(pegWrapper.props().className.split(' ').indexOf('peg-blank')).to.not.equal(-1);
 
       done();
     });
@@ -34,9 +29,8 @@ describe('Peg', function() {
 
   context('with a color passed in as a prop', function() {
     it('has a peg- classname matching the color prop', function(done) {
-      shallowRenderer.render(<Peg color='lavender'/>);
-      var pegComponent = shallowRenderer.getRenderOutput();
-      expect(pegComponent.props.className.split(' ').indexOf('peg-lavender')).to.not.equal(-1);
+      var pegWrapper = shallow(<Peg color='lavender'/>);
+      expect(pegWrapper.props().className.split(' ').indexOf('peg-lavender')).to.not.equal(-1);
 
       done();
     });
