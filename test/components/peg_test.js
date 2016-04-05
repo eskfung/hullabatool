c084ac5eb36667e3aa6d33e70878c6c0e3bdeb8c
Eric Fung <eskfung@gmail.com>
2016-04-04 17:45:29 -0700

    Moving peg tests to components

--- test/components/peg_test.js
+++ test/components/peg_test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import React from 'react';
 import { shallow } from 'enzyme';
-import Peg from '../assets/javascripts/mastermind/peg';
+import Peg from '../../assets/javascripts/mastermind/peg';
 
 describe('Peg', function() {
   it('is a div', function(done) {
