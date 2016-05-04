3ddda0019e290f383516b2bac567bfc609ad796e
Eric Fung <eskfung@gmail.com>
2016-05-03 23:06:53 -0700

    Start adding love letter files

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/mastermind', function(req, res, next) {
   res.render('mastermind', { title: 'Mastermind' });
 });
 
+router.get('/love_letter', function(req, res, next) {
+  res.render('love_letter_player', { title: 'Play Love Letter' });
+});
+
+router.get('/love_letter_cast', function(req, res, next) {
+  res.render('love_letter', { title: 'Love Letter' });
+});
+
 module.exports = router;
