87f3ef07d08784a9be25682a589b34be4c883456
Eric Fung <eskfung@gmail.com>
2016-08-07 21:37:11 -0700

    Removing love letter assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   entry: {
-    mastermind: __dirname + '/assets/javascripts/mastermind.js',
-    love_letter: __dirname + '/assets/javascripts/love_letter.js',
-    love_letter_player: __dirname + '/assets/javascripts/love_letter_player.js'
+    mastermind: __dirname + '/assets/javascripts/mastermind.js'
   },
   output: {
     path: __dirname + '/public/javascripts',
