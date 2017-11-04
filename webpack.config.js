7da6795062997769b9915efb48d4e16f44444159
Eric Fung <ericf@carbonfive.com>
2017-11-03 17:07:27 -0700

    Update babel presets

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['es2015', 'react']
+              presets: ['env', 'stage-3', 'react']
             }
           }
         ]
